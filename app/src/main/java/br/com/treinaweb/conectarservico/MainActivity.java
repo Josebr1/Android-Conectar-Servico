7677345bb46d788e3656362efe2c2310c7251301
José <jose.silva.br@outlook.com>
2016-08-31 21:34:30 -0300

    Conectar Serviço

---
+++ app/src/main/java/br/com/treinaweb/conectarservico/MainActivity.java
@@ -0,0 +1,21 @@
+package br.com.treinaweb.conectarservico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void servico(View view){
+        Intent intent = new Intent("br.com.treinaweb.primeiroprojeto.ACTION.MyService");
+        startService(intent);
+
+    }
+}
